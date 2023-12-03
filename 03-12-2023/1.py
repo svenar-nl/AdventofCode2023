477dabdf86ee2142e5cb9ea21515707530408f12
Sven Arends <github@svenar.nl>
2023-12-03 17:47:57 +0100

    Cleaned-up day 3 code

--- 03-12-2023/1.py
+++ 03-12-2023/1.py
@@ -1,44 +1,59 @@
 file_path = "input.txt"
 
+def extract_digit(char_lines, x, y):
+    found_num = ""
+    x2 = x
 
-def calculate(lines):
+    while x2 >= 0 and char_lines[y][x2].isdigit():
+        found_num = char_lines[y][x2] + found_num
+        x2 -= 1
+
+    x2 = x + 1
+    while x2 < len(char_lines[y]) and char_lines[y][x2].isdigit():
+        found_num += char_lines[y][x2]
+        char_lines[y][x2] = "?"
+        x2 += 1
+
+    return int(found_num)
+
+
+def find_engine_part_numbers(char_lines, x, y, width, height):
+    engine_part_numbers = []
+
+    for y1 in range(-1, 2):
+        for x1 in range(-1, 2):
+            if y1 == 0 and x1 == 0:
+                continue
+
+            if 0 <= y + y1 < height and 0 <= x + x1 < width:
+                if char_lines[y + y1][x + x1].isdigit():
+                    digit = extract_digit(char_lines, x + x1, y + y1)
+                    engine_part_numbers.append(digit)
+
+    return engine_part_numbers
+
+
+def calculate_engine_total(char_lines):
     engine_symbols = "@#$%&*+/=-"
     total = 0
+    number_buffer = []
 
-    char_lines = [[c for c in line] for line in lines]
     width = len(char_lines[0])
     height = len(char_lines)
-
-    number_buffer = []
 
     for y in range(height):
         for x in range(width):
             if char_lines[y][x] in engine_symbols:
-                for y1 in range(-1, 2):
-                    for x1 in range(-1, 2):
-                        if y1 == 0 and x1 == 0:
-                            continue
+                engine_part_numbers = find_engine_part_numbers(char_lines, x, y, width, height)
+                number_buffer.extend(engine_part_numbers)
 
-                        if 0 <= y + y1 < height and 0 <= x + x1 < width:
-                            if char_lines[y + y1][x + x1].isdigit():
-                                found_num = ""
-
-                                x2 = x + x1
-                                while x2 >= 0 and char_lines[y + y1][x2].isdigit():
-                                    x2 -= 1
-
-                                x2 += 1
-                                while x2 < width and char_lines[y + y1][x2].isdigit():
-                                    found_num += char_lines[y + y1][x2]
-                                    char_lines[y + y1][x2] = "?"
-                                    x2 += 1
-                                
-                                number_buffer.append(int(found_num))
-    
     total = sum(number_buffer)
-
     return total
 
+def main(lines):
+    char_lines = [[c for c in line] for line in lines]
+    output = calculate_engine_total(char_lines)
+    print("Answer:", output)
 
 if __name__ == "__main__":
     with open(file_path, "r") as file:
@@ -57,5 +72,4 @@ if __name__ == "__main__":
     #     ".664.598..",
     # ]
 
-    output = calculate(lines)
-    print("Answer:", output)
+    main(lines)
--- 03-12-2023/2.py
+++ 03-12-2023/2.py
@@ -1,43 +1,49 @@
 file_path = "input.txt"
 
+def extract_digit(char_lines, x, y):
+    found_num = ""
+    x2 = x
 
-def calculate(lines):
+    while x2 >= 0 and char_lines[y][x2].isdigit():
+        found_num = char_lines[y][x2] + found_num
+        x2 -= 1
+
+    x2 = x + 1
+    while x2 < len(char_lines[y]) and char_lines[y][x2].isdigit():
+        found_num += char_lines[y][x2]
+        char_lines[y][x2] = "?"
+        x2 += 1
+
+    return int(found_num)
+
+
+def find_gear_part_numbers(char_lines, x, y):
+    gear_part_numbers = []
+
+    for y1 in range(-1, 2):
+        for x1 in range(-1, 2):
+            if y1 == 0 and x1 == 0:
+                continue
+
+            if 0 <= y + y1 < len(char_lines) and 0 <= x + x1 < len(char_lines[y]):
+                if char_lines[y + y1][x + x1].isdigit():
+                    digit = extract_digit(char_lines, x + x1, y + y1)
+                    gear_part_numbers.append(digit)
+
+    return gear_part_numbers
+
+
+def calculate_gear_total(char_lines):
     gear_symbols = "*"
     total = 0
-
-    char_lines = [[c for c in line] for line in lines]
-    width = len(char_lines[0])
-    height = len(char_lines)
-
     number_buffer = []
 
-    for y in range(height):
-        for x in range(width):
+    for y in range(len(char_lines)):
+        for x in range(len(char_lines[y])):
             if char_lines[y][x] == gear_symbols:
-                gear_part_numbers = []
-                for y1 in range(-1, 2):
-                    for x1 in range(-1, 2):
-                        if y1 == 0 and x1 == 0:
-                            continue
-
-                        if 0 <= y + y1 < height and 0 <= x + x1 < width:
-                            if char_lines[y + y1][x + x1].isdigit():
-                                found_num = ""
-
-                                x2 = x + x1
-                                while x2 >= 0 and char_lines[y + y1][x2].isdigit():
-                                    x2 -= 1
-
-                                x2 += 1
-                                while x2 < width and char_lines[y + y1][x2].isdigit():
-                                    found_num += char_lines[y + y1][x2]
-                                    char_lines[y + y1][x2] = "?"
-                                    x2 += 1
-                                
-                                gear_part_numbers.append(int(found_num))
-                
+                gear_part_numbers = find_gear_part_numbers(char_lines, x, y)
                 number_buffer.append(gear_part_numbers)
-    
+
     for gear_pn in number_buffer:
         if len(gear_pn) == 2:
             total += gear_pn[0] * gear_pn[1]
@@ -45,10 +51,16 @@ def calculate(lines):
     return total
 
 
+def main(lines):
+    char_lines = [[c for c in line] for line in lines]
+    output = calculate_gear_total(char_lines)
+    print("Answer:", output)
+
+
 if __name__ == "__main__":
     with open(file_path, "r") as file:
         lines = file.readlines()
-
+    
     # lines = [
     #     "467..114..",
     #     "...*......",
@@ -61,6 +73,4 @@ if __name__ == "__main__":
     #     "...$.*....",
     #     ".664.598..",
     # ]
-
-    output = calculate(lines)
-    print("Answer:", output)
+    main(lines)
